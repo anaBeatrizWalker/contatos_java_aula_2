430872bf9365ebd8f3a72cf68b7d492ff253add1
Ana Beatriz <anabiawalker@gmail.com>
2022-09-26 18:38:25 -0300

    Entidade abstrata

--- src/main/java/br/fatec/contatos/model/Contato.java
+++ src/main/java/br/fatec/contatos/model/Contato.java
@@ -1,21 +1,16 @@
 package br.fatec.contatos.model;
-
-import java.io.Serializable;
+ 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
-@Table (name = "tb_contato")
 @Entity
-public class Contato implements Serializable {
+@Table (name = "tb_contato")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class Contato extends AbstractEntity {
 	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	
 	@Column(length = 60, nullable = true)
 	private String email;
@@ -26,19 +21,7 @@ public class Contato implements Serializable {
 	@Column(length = 20, nullable = true)
 	private String telefone;
 	
-	public Contato() {	}
-	
-	public Contato(Long id) { 
-		this.id = id; 
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
+	public Contato() { }
 
 	public String getEmail() {
 		return email;
@@ -67,29 +50,4 @@ public class Contato implements Serializable {
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Contato other = (Contato) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}	
 }
